c311b280302807ee730df582a0c25cc8c68beb73
ivanapina <ivanapina@gmail.com>
2023-09-26 14:08:31 -0500

    added contact form to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,33 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Page</h1>
+import { MDBInput, MDBCheckbox, MDBBtn, MDBValidation, MDBValidationItem, MDBTextArea} from 'mdb-react-ui-kit';
 
-      </div>
-    );
-    }
+export default function Contact() {
+  return (
+    <MDBValidation noValidate id='form' className='text-center' style={{ width: '100%', maxWidth: '300px', alignContent:'center' }}>
+      <h2>Contact Me</h2>
+
+      <MDBValidationItem invalid feedback='Please provide your name.'>
+        <MDBInput label='Name' v-model='name' wrapperClass='mb-4' required />
+      </MDBValidationItem>
+
+      <MDBValidationItem invalid feedback='Please provide your email.'>
+        <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' required />
+      </MDBValidationItem>
+
+      <MDBValidationItem invalid feedback='Please provide mail subject.'>
+        <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' required />
+      </MDBValidationItem>
+
+      <MDBValidationItem invalid feedback='Please provide a message text.'>
+        <MDBTextArea wrapperClass='mb-4' label='Message' required />
+      </MDBValidationItem>
+
+      <MDBValidationItem feedback=''>
+        <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
+      </MDBValidationItem>
+
+      <MDBBtn type='submit' color='primary' block className='my-4'>
+        Send
+      </MDBBtn>
+    </MDBValidation>
+  );
+}
