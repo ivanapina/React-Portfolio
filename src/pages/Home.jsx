90f00dd52cf825b2d1bc3c86aee3a017fffbbf50
ivanapina <ivanapina@gmail.com>
2023-09-26 12:04:35 -0500

    added profile pic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import profilePic from '/src/assets/profile-pic.png';
+
+export default function Home() {
+    return (
+      <div>
+        <img src={profilePic} alt="Trees" height="200" id='profilePic'/>
+        <h1>About Me</h1>
+        <p>
+          Hello, my name is Ivana and I am a software developer with a background in Fashion and Susteinability!
+          I am motivated in building applications that help make businesses more sustainable by using the latest technology to solve todays issues within the Fashion Industry. 
+        </p>
+      </div>
+    );
+  }
+  
