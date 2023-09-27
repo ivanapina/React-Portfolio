38b693ecf1ad6d93f751261b35a93d6316f1a124
ivanapina <ivanapina@gmail.com>
2023-09-26 22:25:38 -0500

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import profilePic from '/src/assets/profile-pic.png';
 export default function Home() {
     return (
       <div className="container text-center mb-5">
-        <img src={profilePic} alt="Profile Picture" height="200"/>
+        <img src={profilePic} alt="Profile Picture" height="200" style={{paddingTop: 20}}/>
         <h1>About Me</h1>
         <p>
           Hello, my name is Ivana and I am a software developer with a background in Fashion and Sustainability!
